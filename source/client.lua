6c4af3aebd2cbbf304c09c51ed3585a1826b86dd
BreezyTheDev <63890993+BreezyTheDev@users.noreply.github.com>
2022-08-30 11:12:12 -0700

    v1.3
    Fixed webhook exploitation.

--- source/client.lua
+++ source/client.lua
@@ -1,5 +1,5 @@
 --================================--
---      Keybased-AC 1.2           --
+--      Keybased-AC 1.3           --
 --      by BreezyTheDev           --
 --		GNU License v3.0		  --
 --================================--
@@ -15,16 +15,9 @@ Citizen.CreateThread(function()
             local keyMsg = suspiciouskeys[i][2];
             local keyMsg2 = suspiciouskeys[i][3];
             local keybind1 = Keybind[1];
-            if IsControlJustReleased(0, keybind1) then
+            if IsControlJustPressed(0, keybind1) then
                 Counter = Counter + 1;
-                if Counter == Config.DiscordLogs.Counter then
-                    if Config.DiscordLogs.Screenshot then
-                        exports['screenshot-basic']:requestScreenshotUpload(Config.DiscordLogs.Webhook, 'files[]', function(data)
-                            resp = json.decode(data)
-                            SendNUIMessage({ action = "updateIMG", img = resp.attachments[1].proxy_url  })
-                            CACHE_IMG = resp.attachments[1].proxy_url
-                        end)
-                    end
+                if Counter == Config.Options.Counter then
                     TriggerServerEvent("KeybasedAC:Pressed", keyMsg, keyMsg2)
                     Counter = 0
                 end
@@ -32,6 +25,3 @@ Citizen.CreateThread(function()
         end
     end
 end)
-
-
-
