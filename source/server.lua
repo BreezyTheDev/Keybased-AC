f49063bae3a24b423a13beabc689b1964f8618d6
BreezyTheDev <63890993+BreezyTheDev@users.noreply.github.com>
2022-08-30 11:10:58 -0700

    v1.3
    Fixed webhook exploitation.

--- source/server.lua
+++ source/server.lua
@@ -8,31 +8,48 @@ RegisterNetEvent("KeybasedAC:Pressed")
 AddEventHandler("KeybasedAC:Pressed", function(keyMsg, keyMsg2)
     local src = source
     local author = GetPlayerName(src)
-    sendToDisc("Suspicious Activity by: ["..author.."] (ID: "..src..")", 
-            "**Notice:** User is pressing keys commonly used by third-party menus.\n**Key Pressed:** ``".. keyMsg.."`` or ``"..keyMsg2.."``\n**Counter:** ``"..Config.DiscordLogs.Counter.."``")
+    if Config.Screenshot then
+        exports["discord-screenshot"]:requestCustomClientScreenshotUploadToDiscord(
+        GetPlayers()[1],
+        Config.DiscordWebhook,
+        {
+            encoding = "png",
+            quality = 1
+        },
+        {
+            embeds = {
+                {
+                    ["color"] = 13055799,
+                    ["title"] = "Suspicious Activity by: [" .. author.."] (ID: " .. src .. ")",
+                    ["description"] = "**Notice:** User is pressing keys commonly used by third-party menus.\n**Key Pressed:** ``"..keyMsg.."`` or ``"..keyMsg2.."``",
+                    ["footer"] = {
+                        ["text"] = "Keybased-AC 1.3 by Breezy#0001",
+                    },
+                }
+            }
+        },
+        30000,
+        )
+    else
+        sendToDisc("Suspicious Activity by: ["..author.."] (ID: "..src..")", 
+            "**Notice:** User is pressing keys commonly used by third-party menus.\n**Key Pressed:** ``".. keyMsg.."`` or ``"..keyMsg2.."``")
+    end
 end)
 
+-- If Config.Screenshot was set to false then
 function sendToDisc(title, msg)
-    local embed = {}
-    embed = {
+    local embed = {
         {
             ["color"] = 13055799,
             ["title"] = "**".. title .."**",
             ["description"] = msg,
             ["footer"] = {
-                ["text"] = "Keybased AC 1.0 by Breezy#0001",
+                ["text"] = "Keybased-AC 1.3 by Breezy#0001",
             },
         }
     }
-    PerformHttpRequest(Config.DiscordLogs.Webhook,
-    function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
+    PerformHttpRequest(Config.DiscordWebhook, function(err, text, headers) end, 'POST', json.encode({username = name, embeds = embed}), { ['Content-Type'] = 'application/json' })
   -- END
 end
 
 print("^0This resource was created by ^5Breezy#0001 ^0for support you can join his ^5discord: ^0https://discord.gg/zzUfkfRHzP")
-
-if Settings.Debug.Enable then
-    if Settings.DiscordLogs.Webhook == '' then
-        print("^1[Keybased-AC Debug] ^3In order for discord logs to work properly a webhook needs to be added in the config. ^7")
-    end
-end
