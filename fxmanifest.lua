2d0d1fee37ec3678f91af5f5ecfb13d8f3b0e5aa
BreezyTheDev <63890993+BreezyTheDev@users.noreply.github.com>
2022-08-30 10:49:58 -0700

    v1.3
    Fixed webhook exploitation.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,12 +1,18 @@
 -- For support join my discord: https://discord.gg/zzUfkfRHzP
 author "Breezy#0001"
 description "Keybased-AC"
-version "1.2"
+version "1.3"
 
 fx_version "cerulean"
 game "gta5"
 lua54 "yes"
 
-shared_script "config.lua"
-server_script "source/server.lua"
-client_script "source/client.lua"
+server_scripts {
+    "sv_config.lua",
+    "source/server.lua"
+}
+
+client_scripts {
+    "cl_config.lua",
+    "source/client.lua"
+}
